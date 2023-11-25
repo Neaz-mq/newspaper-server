429192bc58aefd71c68753496d3db29ecdc93ed3
Neaz-mq <neazmorshed666@gmail.com>
2023-11-25 19:52:25 +0600

    read the data for details of article

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -81,6 +81,19 @@ async function run() {
       res.send(result);
 
     })
+
+    app.get('/article/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await articleCollection.findOne(query);
+      res.send(result);
+
+
+    })
+
+
+
+  
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
