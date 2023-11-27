029cfe289bc2e0c50ff53b2b85308fa72108e26c
Neaz-mq <neazmorshed666@gmail.com>
2023-11-27 14:12:47 +0600

    get the individual user

--- index.js
+++ index.js
@@ -70,6 +70,18 @@ async function run() {
       res.send(users)
 
     })
+
+    app.get('/user/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.findOne(query);
+      res.send(result);
+
+
+    })
+
+
+
     app.post('/user', async (req, res) => {
       const user = req.body;
       console.log(user);
